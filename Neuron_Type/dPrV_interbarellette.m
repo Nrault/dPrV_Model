851fa59e31cc7c68dc01e354e503cae29fe50114
Nrault <nicolas.rault97@gmail.com>
2021-06-16 14:58:59 +0200

    Adding skeleton of the interbarrelletes neurons
    Adding interbarrellette neuron with T-type and H-type conductances

--- Neuron_Type/dPrV_interbarellette.m
+++ Neuron_Type/dPrV_interbarellette.m
@@ -8,42 +8,43 @@ Tvec = Tmin:dt:Tmax;
 
 %Neuron parameters
 Gl = 3e-8;
-maxGna = 1.2e-5;
-maxGk = 2e-6;
-maxGh = 200e-9;
-maxGa = 4.77e-6;
+%Gna = 1.2e-5;
+%Gk = 2e-6;
+Gh = 200e-9;
+Gt = 0.22e-6;
 
 El = -0.060;
 Eh = -0.020;
-Ena = 0.055;
-Ek = -0.072;
-Ea = -0.075;
+%Ena = 0.055;
+%Ek = -0.072;
+Eca = 120e-3;
 
-Cm = 1/(633e6);
+Cm = 1/(417e6);
+
 
 Iapp = zeros(1, length(Tvec)) * 850e-12;
 Vvec = zeros(1, length(Tvec));
 Vvec(1) = El;
 
-m = zeros(1, length(Tvec));
-h = zeros(1, length(Tvec));
-n = zeros(1, length(Tvec));
+%m = zeros(1, length(Tvec));
+%h = zeros(1, length(Tvec));
+%n = zeros(1, length(Tvec));
 m_h = zeros(1, length(Tvec));
-a = zeros(1, length(Tvec));
-b = zeros(1, length(Tvec));
+m_t = zeros(1, length(Tvec));
+h_t = zeros(1, length(Tvec));
 
 Il = zeros(1, length(Tvec));
-Ina = zeros(1, length(Tvec));
-Ik = zeros(1, length(Tvec));
-Ia = zeros(1, length(Tvec));
+%Ina = zeros(1, length(Tvec));
+%Ik = zeros(1, length(Tvec));
 Ih = zeros(1, length(Tvec));
+It = zeros(1, length(Tvec));
 
-m(1) = 0.0;
-h(1) = 0.0;
-n(1) = 0.0;
+%m(1) = 0.0;
+%h(1) = 0.0;
+%n(1) = 0.0;
 m_h(1) = 0.0;
-a(1) = 0.0;
-b(1) = 0.0;
+m_t(1) = 0.0;
+h_t(1) = 0.0;
 
 %Setup current pulses
 for i = 1:length(Tvec)
@@ -56,42 +57,43 @@ for i = 2:length(Tvec)-1
     %Compute derivative of membrane potential
     Il(i-1) = (Gl * (El - Vvec(i-1)));
     %Ina(i-1) = (maxGna * m(i-1)^3 * h(i-1) * (Ena - Vvec(i-1)));
-    Ina(i-1) = 0;
+    %Ina(i-1) = 0;
     %Ik(i-1) = (maxGk * n(i-1)^4 * (Ek - Vvec(i-1)));
-    Ik(i-1) = 0;
-    Ia(i-1) = (maxGa * a(i-1)^3 * b(i-1) * (Ea - Vvec(i-1)));
-    Ih(i-1) = (maxGh * m_h(i-1) * (Eh - Vvec(i-1)));
-    dvdt = (Il(i-1) + Ina(i-1) + Ik(i-1) + Ia(i-1) + Ih(i-1) + Iapp(i-1))/Cm;
+    %Ik(i-1) = 0;
+    It(i-1) = (Gt * (m_t(i-1)^2) * h_t(i-1) * (Eca - Vvec(i-1)));
+    Ih(i-1) = (Gh * m_h(i-1) * (Eh - Vvec(i-1)));
+    dvdt = (Il(i-1) + It(i-1) + Ih(i-1) + Iapp(i-1))/Cm;
     
     
     %Compute gating variables
-    [alpha_m, beta_m] = compute_gating_var_m(Vvec(i-1));
-    [alpha_h, beta_h] = compute_gating_var_h(Vvec(i-1));
-    [alpha_n, beta_n] = compute_gating_var_n(Vvec(i-1));
+    %[alpha_m, beta_m] = compute_gating_var_m(Vvec(i-1));
+    %[alpha_h, beta_h] = compute_gating_var_h(Vvec(i-1));
+    %[alpha_n, beta_n] = compute_gating_var_n(Vvec(i-1));
+    m_h(i) = compute_m_t_inf(Vvec(i-1));
+    [h_t_inf, tau_h_t] = compute_h_t_var(Vvec(i-1));
     
-    tau_m = 1./(alpha_m+beta_m);      % time constant converted from ms to sec
-    m_inf = alpha_m./(alpha_m+beta_m);
+    %tau_m = 1./(alpha_m+beta_m);      % time constant converted from ms to sec
+    %m_inf = alpha_m./(alpha_m+beta_m);
 
-    tau_h = 1./(alpha_h+beta_h);      % time constant converted from ms to sec
-    h_inf = alpha_h./(alpha_h+beta_h);
+    %tau_h = 1./(alpha_h+beta_h);      % time constant converted from ms to sec
+    %h_inf = alpha_h./(alpha_h+beta_h);
 
-    tau_n = 1./(alpha_n+beta_n);      % time constant converted from ms to sec
-    n_inf = alpha_n./(alpha_n+beta_n);
+    %tau_n = 1./(alpha_n+beta_n);      % time constant converted from ms to sec
+    %n_inf = alpha_n./(alpha_n+beta_n);
     
     [m_h_inf, tau_m_h] = compute_var_H(Vvec(i-1));
     %Compute derivative of gating variables
-    dmhdt = (m_h_inf - m_h(i-1))/tau_m_h; 
-    [dadt, dbdt] = compute_gating_var_A(Vvec(i-1), a(i-1), b(i-1));
+    dhtdt = (h_t_inf - h_t(i-1)) / tau_h_t;
+    dmhdt = (m_h_inf - m_h(i-1)) / tau_m_h; 
     
     %Compute the value of each variable of the next time step
     Vvec(i) = Vvec(i-1) + dt * dvdt;
     
-    m(i) = m(i-1) + (m_inf-m(i-1))*dt/tau_m;    % Update m
-    h(i) = h(i-1) + (h_inf-h(i-1))*dt/tau_h;    % Update h
-    n(i) = n(i-1) + (n_inf-n(i-1))*dt/tau_n;    % Update n
+    %m(i) = m(i-1) + (m_inf-m(i-1))*dt/tau_m;    % Update m
+    %h(i) = h(i-1) + (h_inf-h(i-1))*dt/tau_h;    % Update h
+    %n(i) = n(i-1) + (n_inf-n(i-1))*dt/tau_n;    % Update n
     m_h(i) = m_h(i-1) + dt * dmhdt;
-    a(i) = a(i-1) + dt * dadt;
-    b(i) = b(i-1) + dt * dbdt;
+    h_t(i) = h_t(i-1) + dt * dhtdt;
     
 end
 
@@ -143,40 +145,27 @@ function beta_n = compute_beta_n(Vm)
     beta_n = 250*exp(-12.5*(Vm+0.0557));
 end
 
-%Compute A-type conductance
-function [dadt, dbdt] = compute_gating_var_A(Vm, a, b)
-    dadt = compute_dadt(Vm, a);
-    dbdt = compute_dbdt(Vm, b);
+%Compute T-type conductance
+function m_t_inf = compute_m_t_inf(Vm)
+    m_t_inf = 1 / (1 + exp(-(Vm + 0.052)/0.0074));
 end
 
-function dadt = compute_dadt(Vm, a)
-    tau_a = compute_tau_a(Vm);
-    a_inf = compute_a_inf(Vm);
-    dadt = (a_inf - a) / tau_a; 
+function [h_t_inf, tau_h_t] = compute_h_t_var(Vm)
+    h_t_inf = compute_h_t_inf(Vm);
+    tau_h_t = compute_tau_h_t(Vm);
 end
 
-function tau_a = compute_tau_a(Vm)
-    tau_a = 0.3632e-3 + 1.158e-3./(1+exp(49.7*(Vm+0.05596)));
+function h_t_inf = compute_h_t_inf(Vm)
+    h_t_inf = 1/(1 + exp(500 * (Vm + 0.0076)));
 end
 
-function a_inf = compute_a_inf(Vm)
-    a_inf = (0.0761*exp(31.4*(Vm+0.09422))./(1+exp(34.6*(Vm+0.00117)))).^(1/3.0);
+function tau_h_t = compute_tau_h_t(Vm)
+    if(Vm < - 0.080)
+        tau_h_t = 0.001 * exp(15 * (Vm + 0.467)); 
+    else
+        tau_h_t = 0.028 + 0.001 * exp(-(Vm + 0.022)/0.0105);
+    end
 end
-
-function dbdt = compute_dbdt(Vm, b)
-    tau_b = compute_tau_b(Vm);
-    b_inf = compute_b_inf(Vm);
-    dbdt = (b_inf - b) / tau_b;
-end
-
-function tau_b = compute_tau_b(Vm)
-    tau_b = 1.24e-3 + 2.678e-3./(1+exp(62.4*(Vm+0.050)));
-end
-
-function b_inf = compute_b_inf(Vm)
-    b_inf = (1./(1+exp(68.8*(Vm+0.0533)))).^4;
-end
-
 %Compute H-type conductance
 function [m_h_inf, tau_m_h] = compute_var_H(Vm)
     m_h_inf = compute_m_h_inf(Vm);
